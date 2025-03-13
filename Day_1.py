152c2d83c9ab1a85fd134c9f8c06dc3070a70ea6
Keerthana17-hash <keerthanagowlikar17@gmail.com>
2025-03-13 11:11:11 +0530

    Create Day_1.py

---
+++ Day_1.py
@@ -0,0 +1,13 @@
+#
+num=int(input("Enter the number: "))
+if num>=100:
+    print("The given number is greater than 100:",num)
+#
+string=input("Enter the string: ")
+if len(string) % 2==0:
+    print("The given string having the even characters")
+
+#
+string=input("Enter the string: ")
+if len(string) % 2-1==0:
+    print("The given string having the odd characters")
